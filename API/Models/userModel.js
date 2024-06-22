c004d1f7d24cb56b2d731078ed0318bdb2732f6b
navamayoo <navamayoo@gmail.com>
2024-06-22 11:31:32 +0530

    Create user Module

---
+++ API/Models/userModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    googleId: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
+    name: { type: String, required: true },
+    picture: { type: String }
+});
+
+module.exports = mongoose.model('User', userSchema);

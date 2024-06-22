203b73cb6436d2f5e8c9edad4ed369ac6ae276f7
navamayoo <navamayoo@gmail.com>
2024-06-22 11:32:13 +0530

    Create handelLogin for API call

--- GUI/src/google-Oauth/Login.jsx
+++ GUI/src/google-Oauth/Login.jsx
@@ -9,9 +9,34 @@ import { jwtDecode } from "jwt-decode";
 const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 export default function login() {
-  const handelLogin = (googleData) => {
-    const userData = jwtDecode(googleData);
-    console.log(userData);
+
+  const [loginData, setLoginData] = useState(
+    localStorage.getItem("loginData")
+      ? JSON.parse(localStorage.getItem("loginData"))
+      : null
+  );
+
+
+  const handelLogin = async (googleData) => {
+    //const userData = jwtDecode(googleData);
+
+    try {
+      const res = await fetch("http://localhost:5000/api/google-login", {
+        method: "POST",
+        body: JSON.stringify({token: googleData, }),
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (!res.ok) {
+        throw new Error("Failed to log in with Google");
+      }
+
+      const data = await res.json();
+      setLoginData(data);
+      localStorage.setItem("loginData", JSON.stringify(data));
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
   };
 
   return (
@@ -19,8 +44,6 @@ export default function login() {
       <button type="button">
 
         <GoogleOAuthProvider clientId={CLIENT_ID}>
-
-          
           <GoogleLogin
             onSuccess={(credentialResponse) => {
               handelLogin(credentialResponse.credential);
